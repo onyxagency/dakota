943f45afa42c7ca2b4570d53dbd597f50ca4f141
Zach Nolan <zach@uplinkconsulting.com>
2015-02-26 11:34:03 -0800

    setup deployment process

---
+++ config/unicorn.rb
@@ -0,0 +1,17 @@
+if ENV["RAILS_ENV"] == "production"
+	root = "/var/www/getdakota.com/current"
+else
+	root = "/var/www/staging.getdakota.com/current"
+end
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+if ENV["RAILS_ENV"] == "production"
+	listen "/tmp/unicorn.dakota.sock"
+else
+	listen "/tmp/unicorn.dakota_staging.sock"
+end
+worker_processes 1
+timeout 30
